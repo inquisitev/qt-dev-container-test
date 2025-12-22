229317b07eb695f074bb016311af1580b7c0b412
Trevor Keegan <positev@outlook.com>
2025-12-22 12:09:52 -0500

    can clone private dotfiles repo

--- startDevContainer.sh
+++ startDevContainer.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 
-devcontainer exec --workspace-folder .  rm /root/.devcontainer/.dotfilesMarker             
-devcontainer exec --workspace-folder . rm ~/.local/share/chezmoi
-devcontainer exec --workspace-folder . rm ~/dotfiles
-devcontainer exec --workspace-folder . chezmoi state delete-bucket --bucket=scriptState
-
-devcontainer build --workspace-folder . \
---no-cache
-
 devcontainer up --workspace-folder . \
---dotfiles-repository "inquisitev/dotfiles" \
---dotfiles-target-path "~/.local/share/chezmoi"
+--remove-existing-container 
+
+devcontainer exec --workspace-folder . bash -lc \
+'DOTFILES=$HOME/.local/share/chezmoi; \
+ mkdir -p $HOME/.ssh $HOME/.local/share; \
+ ssh-keyscan github.com >> $HOME/.ssh/known_hosts; \
+ chmod 700 $HOME/.ssh; chmod 600 $HOME/.ssh/known_hosts; \
+ if [ ! -d "$DOTFILES/.git" ]; then \
+   sudo git clone git@github.com:inquisitev/dotfiles.git "$DOTFILES" && "$DOTFILES/install.sh"; \
+ else \
+   sudo /usr/bin/chezmoi update
+ fi'
+
