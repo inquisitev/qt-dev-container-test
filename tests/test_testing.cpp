a1d5cd040e8348a0e4a952b8870a241cadba2fb2
Trevor Keegan <positev@outlook.com>
2025-12-22 19:36:11 -0500

    setup autotest

--- tests/test_testing.cpp
+++ tests/test_testing.cpp
@@ -1,5 +1,5 @@
 #include "./test_testing.h"
 
-void TestBasicAssertion::basic_math_failure() { QCOMPARE(1, 2); }
+void TestBasicAssertion::basic_math_failure() { QCOMPARE(2, 2); }
 
 QTEST_APPLESS_MAIN(TestBasicAssertion)
